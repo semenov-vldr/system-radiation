79fd0e3174fc031a5e6c46a1e379a1fc202ac669
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-12-04 21:18:55 +0000

    deploy: 2d7a83af09d20ab67f14cd36aac6aa6b419814c8

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -141,46 +141,6 @@ if (values) {
 }
 "use strict";
 
-var mySwiper;
-var swiperClients = document.querySelector('.clients__box');
-mySwiper = new Swiper(swiperClients, {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    types: "bullets"
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true
-  },
-  // Откл функционала, если слайдов меньше, чем нужно
-  watchOverflow: true,
-  // Брейк поинты (адаптив)
-  breakpoints: {
-    320: {
-      slidesPerView: 2.5,
-      spaceBetween: 16
-    },
-    480: {
-      slidesPerView: 3.5,
-      spaceBetween: 16
-    },
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 32
-    },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 32
-    },
-    1100: {
-      slidesPerView: 6,
-      spaceBetween: 32
-    }
-  }
-});
-"use strict";
-
 var accordionItems = document.querySelectorAll('.accordion__item'); // список элементов аккордиона
 
 if (accordionItems) {
