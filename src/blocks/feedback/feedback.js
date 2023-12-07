6d56a268a521b5c80ede5b34a06ec901e95d01f9
Vladimir <vova2066@yandex.ru>
2023-12-08 01:01:00 +0300

    add message feedback

--- src/blocks/feedback/feedback.js
+++ src/blocks/feedback/feedback.js
@@ -4,36 +4,74 @@ const TOKEN = "6439049822:AAHuQyECo9HqHDpzRcj9qwrt384oisaNJYY";
 const CHAT_ID = "-1002094796235";
 const URL_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 
-const form = document.getElementById("feedback");
+const feedback = document.getElementById("feedback");
 
-if (form) form.addEventListener('submit', (evt) => sendMsgTelegram(evt));
+if (feedback) {
+  feedback.addEventListener('submit', (evt) => sendMsgTelegram(evt));
 
 
-function sendMsgTelegram (evt) {
-  evt.preventDefault();
-  const form = evt.target;
+  const form = feedback.querySelector(".feedback__form");
+  const messageSuccess = feedback.querySelector(".feedback-message__body-success");
+  const messageError = feedback.querySelector(".feedback-message__body-error");
 
-  let message = `<b>Заявка с сайта ООО Система - Радиационное оборудование</b>\n`;
 
-  message += `<b>Имя отправителя:</b> ${ form.name.value }\n`;
-  message += `<b>Телефон:</b> ${ form.phone.value }\n`;
+  function submitSuccess () {
+    console.log("Success")
+    form.classList.add("hidden");
+    messageSuccess.classList.add("visible");
 
-  //message += `<b>Сообщение:</b> ${ this.message.value }\n`;
+    setTimeout(() => {
+      form.classList.remove("hidden");
+      messageSuccess.classList.remove("visible");
+      form.reset();
+    }, 5000);
+  }
 
-  axios.post(URL_API, {
-    chat_id: CHAT_ID,
-    parse_mode: 'html',
-    text: message,
-  })
-    .then(() => {
-      console.log('Заявка успешно отправлена');
+  function submitError () {
+    console.log("Error")
+    form.classList.add("hidden");
+    messageError.classList.add("visible");
+
+    setTimeout(() => {
+      form.classList.remove("hidden");
+      messageError.classList.remove("visible");
+      form.reset();
+    }, 5000);
+  }
+
+
+  function sendMsgTelegram (evt) {
+    evt.preventDefault();
+    const form = evt.target;
+
+    let message = `<b>Заявка с сайта ООО Система - Радиационное оборудование</b>\n`;
+
+    message += `<b>Имя отправителя:</b> ${ form.name.value }\n`;
+    message += `<b>Телефон:</b> ${ form.phone.value }\n`;
+
+    axios.post(URL_API, {
+      chat_id: CHAT_ID,
+      parse_mode: 'html',
+      text: message,
     })
-    .catch((err) => {
-      console.warn(err);
-    })
-    .finally(() => {
-      console.log('Конец');
-    });
-  alert('Заявка отправлена успешно');
-  form.reset()
-};
+      .then(() => {
+        console.log('Заявка успешно отправлена');
+        submitSuccess();
+      })
+      .catch((err) => {
+        console.warn(err);
+        submitError();
+      })
+      .finally(() => {
+        console.log('Конец');
+      });
+  };
+
+
+
+}
+
+
+
+
+
