0652b62d6dc8d829827e757240b16952898c62ee
Vladimir <vova2066@yandex.ru>
2024-01-11 23:09:13 +0300

    add email popup

--- src/blocks/popup/popup.js
+++ src/blocks/popup/popup.js
@@ -58,6 +58,9 @@ function sendMsgTelegram (evt) {
 
   message += `<b>Имя отправителя:</b> ${ form.name.value }\n`;
   message += `<b>Телефон:</b> ${ form.phone.value }\n`;
+  if (form.email.value) {
+    message += `<b>Почта:</b> ${ form.email.value }\n`;
+  }
 
   axios.post(URL_API, {
     chat_id: CHAT_ID,
